14c16951da227a2071b909e769c7709a5ea1f311
NicoCortese <nicolasacortese@gmail.com>
2022-01-06 22:11:09 -0500

    Third attempt at save_csv function, written outside of save_qualifying_loans function so it can also be used elsewhere

--- Starter_Code/loan_qualifier_app/app.py
+++ Starter_Code/loan_qualifier_app/app.py
@@ -102,8 +102,13 @@ def find_qualifying_loans(bank_data, credit_score, debt, income, loan, home_valu
 
     return bank_data_filtered
 
+# function that saves info to a new file
 def save_csv(qualifying_loans):
-    
+    csvpath = Path("Starter_Code/loan_qualifier_app/qualifying_loans.csv")
+    with open(csvpath, 'w', newline='') as csvfile:
+        csvwriter=csv.writer(csvfile)
+        for row in qualifying_loans:
+            csvwriter.writerow(row)
 
 def save_qualifying_loans(qualifying_loans):
     """Saves the qualifying loans to a CSV file.
@@ -112,11 +117,7 @@ def save_qualifying_loans(qualifying_loans):
         qualifying_loans (list of lists): The qualifying bank loans.
     """
     # @TODO: Complete the usability dialog for savings the CSV Files.
-    csvpath = Path("Starter_Code/loan_qualifier_app/qualifying_loans.csv")
-    with open(csvpath, 'w', newline='') as csvfile:
-        csvwriter=csv.writer(csvfile)
-        for row in qualifying_loans:
-            csvwriter.writerow(row)
+    
 
 
 def run():
